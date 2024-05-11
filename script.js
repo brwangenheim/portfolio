87c3d0532589944d613a8aa4a2460cb873c63885
brwangenheim <brooke_wangenheim@brown.edu>
2024-05-10 21:10:50 -0400

    yay

--- script.js
+++ script.js
@@ -2,23 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll("nav a");
   links.forEach((link) => {
     link.addEventListener("click", function (event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href");
-      const targetSection = document.querySelector(targetId);
-      targetSection.scrollIntoView({ behavior: "smooth" });
+      const linkUrl = new URL(this.href);
+      const currentPageUrl = new URL(window.location.href);
+      if (
+        linkUrl.origin === currentPageUrl.origin &&
+        linkUrl.pathname === currentPageUrl.pathname
+      ) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+          targetSection.scrollIntoView({ behavior: "smooth" });
+        }
+      }
     });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const header = document.getElementById("header");
-  const aboutSection = document.getElementById("about");
-
-  window.addEventListener("scroll", function () {
-    if (window.scrollY >= aboutSection.offsetTop - 150) {
-      header.classList.add("solid");
-    } else {
-      header.classList.remove("solid");
-    }
   });
 });
